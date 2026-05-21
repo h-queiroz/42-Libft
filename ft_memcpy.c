422a9c989b6ff97f6aa5ce1a6f477aea4b59b12e
Hector Queiroz Ribeiro Dos Santos <hequeiro@c3r7p4.42sp.org.br>
2026-05-21 18:01:07 -0300

    ft_memcpy updated

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -14,18 +14,16 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	char			*my_dest;
-	const char		*my_src;
+	char		*my_dest;
+	const char	*my_src;
 	unsigned int	i;
 
-	my_dest = dest;
-	my_src = src;
 	i = 0;
+	my_dest = dest;
+	my_src 	= src;
 	while (i < n)
 	{
-		*my_dest = *my_src;
-		my_dest++;
-		my_src++;
+		my_dest[i] = my_src[i];
 		i++;
 	}
 	return (dest);
