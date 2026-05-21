0baf782921eb0ad0c72fee7548502f6b7e2d3878
h-queiroz <hectorqrs@live.com>
2026-05-20 22:24:06 -0300

    ft_strdup fixed to take in consideration space for the null terminator

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: hector <hequeiro@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 20:25:02 by hector            #+#    #+#             */
-/*   Updated: 2026/05/19 20:34:44 by hector           ###   ########.fr       */
+/*   Updated: 2026/05/20 22:22:39 by hector           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	*ft_strdup(const char *s)
 	length = ft_strlen(s);
 	if (length <= 0)
 		return (NULL);
-	result = malloc(sizeof(char) * length);
+	result = malloc(sizeof(char) * length + 1);
 	if (!result)
 		return (NULL);
 	while (s[i])
